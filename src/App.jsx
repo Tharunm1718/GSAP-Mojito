b677b4809f8a87fbf1724a4e36ed2cfc1dd26536
Tharunm1718 <tharunm1817@gmail.com>
2026-04-19 13:27:58 +0530

    Setup:Tailwind and GSAP

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap"
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+function App()
+{
+  return(
+    <p>APP</p>
+  )
+}
+
+export default App
